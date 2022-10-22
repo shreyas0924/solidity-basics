826c5894167d16f954122bfbd658d55020c960e5
shreyas0924 <shreyasp0924@gmail.com>
2022-10-22 20:29:03 +0530

    updated simple storage

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,45 +1,32 @@
-//SPDX-License-Identifier: MIT
+
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
-contract SimpleStorage {
-    uint256 public number; 
-    function store(uint256 _number) public {
-        number = _number;
-    } 
 
-    mapping(string => uint256) public nameToNumber;
+contract SimpleStorage {
+
+    uint256 favoriteNumber;
 
     struct People {
-        uint256 number;
+        uint256 favoriteNumber;
         string name;
     }
-
+    // uint256[] public anArray;
     People[] public people;
-    //calldata , memory , storage
-    function addPerson(string memory _name, uint256 _number) public {
-        people.push(People(_number, _name));
-        nameToNumber[_name] = _number;
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public virtual {
+        favoriteNumber = _favoriteNumber;
+    }
+    
+    function retrieve() public view returns (uint256){
+        return favoriteNumber;
     }
 
-    // view means read the state from this contract
-
-    function retrieve() public view returns(uint256){
-        return number;
-    } 
-
-    //retrieve is just reading the number
-    // We cannot update the blockchain with a view function
-
-    //Pure functions does not allow you to read the state from the blockchain ex. we cannot read "number" if we use pure as keyword 
-    // Pure is used to write specific code whch is only called and not read
-    function add() public pure returns(uint256) {
-        return (1 + 1);
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
     }
-
-    //We dont spend gas if we use pure or view functions
-    //We spend gas only if there is a change in state in the blockchain
-
-
-
-
 }
