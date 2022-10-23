a3f41c0ecea335abb1d96905e2a3860fd79a6d0e
shreyas0924 <shreyasp0924@gmail.com>
2022-10-23 19:48:39 +0530

    added getversion using chianlnk interface

--- FundMe/contracts/FundMe.sol
+++ FundMe/contracts/FundMe.sol
@@ -1,7 +1,9 @@
 //SPDX-License-Identifier: MIT
  
 //Get funds from user, wihtdraw funds from user , and set a min funding value in USD
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe { 
     uint256 public minUsd = 50;
@@ -10,10 +12,20 @@ contract FundMe {
 
     }
 
-    function getPrice() { //Get the price of ethereum in terms of usd
-
+    function getPrice() public { //Get the price of ethereum in terms of usd
+        //abi
+        //address 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
 
     }
+    // function getConversionRate() public {
+
+    // }
+
+    function getVersion() public view returns(uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+        return priceFeed.version();
+    }
+
     // function wihtdraw() {
     // }
 
