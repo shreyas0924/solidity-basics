2b9484bd91435f2ef398fb0b4132843f84adee4a
shreyas0924 <shreyasp0924@gmail.com>
2022-10-25 21:46:31 +0530

    added conversion rate

--- FundMe/contracts/FundMe.sol
+++ FundMe/contracts/FundMe.sol
@@ -6,20 +6,25 @@ pragma solidity ^0.8.7;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe { 
-    uint256 public minUsd = 50;
+    uint256 public minUsd = 50 * 1e18 ;
     function fund() public payable {
-        require(msg.value > minUsd, "Not enough ether") ;
+        require(getConversionRate(msg.value) > minUsd, "Not enough ether") ;
 
     }
 
-    function getPrice() public { //Get the price of ethereum in terms of usd
+    function getPrice() public view returns(uint256) { //Get the price of ethereum in terms of usd
         //abi
         //address 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
-
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+        (, int256 price,  , , ) = priceFeed.latestRoundData();
+        return uint256(price ** 1e10);
     }
-    // function getConversionRate() public {
 
-    // }
+    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18 ;
+        return ethAmountInUsd; 
+    }
 
     function getVersion() public view returns(uint256) {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
